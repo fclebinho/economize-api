fc355b4f69625c80511608131502e1ed7e9e3d2d
Cleber Gomes <fclebinho@gmail.com>
2021-06-04 23:05:03 -0300

    Keycloak

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::API
+	def current_user
+    keycloak_id = Keycloak::Helper.current_user_id(request.env)
+    # User.active.find_by(keycloak_id: keycloak_id)
+  end
 end
